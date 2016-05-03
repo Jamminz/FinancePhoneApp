2e61d7276a3c2684fabb8cc47912ec27760a317b
jim <jimgotslim@hotmail.com>
2016-05-03 12:40:17 +0100

    expenditure delete functionality added

--- PhoneApp1/Views/Expenditure.xaml.cs
+++ PhoneApp1/Views/Expenditure.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using PhoneApp1.Model;
 
 namespace PhoneApp1.Views
 {
@@ -15,11 +16,33 @@ namespace PhoneApp1.Views
         public Expenditure()
         {
             InitializeComponent();
+
+            using (AppDataContext context = new AppDataContext(AppDataContext.DbConnectionString))
+            {
+                var allExpenditures = context.Expenditures;
+
+                foreach (var i in allExpenditures)
+                {
+                    overviewTextBlock.Text = overviewTextBlock.Text + "ID: " + i.ExpenditureId + "  Amount: £" + i.Amount + "   Description: " + i.Description + "\n";
+                }
+
+            }
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/Views/ExpenditureAdd.xaml", UriKind.Relative));
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Dashboard.xaml", UriKind.Relative));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/ExpenditureDelete.xaml", UriKind.Relative));
         }
     }
 }
---
+++ PhoneApp1/Views/ExpenditureDelete.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Model;
+
+namespace PhoneApp1.Views
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (AppDataContext context = new AppDataContext(AppDataContext.DbConnectionString))
+            {
+
+                bool doesExist = context.Expenditures.Any(o => o.ExpenditureId == Int32.Parse(IdTextBox.Text)
+                                                                 && o.CreatedBy == GlobalClass.whoIsLoggedIn);
+
+                var delExpenditure = 
+                    context.Expenditures.Where(o => o.ExpenditureId == Int32.Parse(IdTextBox.Text) 
+                    && o.CreatedBy == GlobalClass.whoIsLoggedIn)
+                    .ToList();
+
+                    if (doesExist)
+                    {
+                        foreach (var del in delExpenditure)
+                        {
+                            context.Expenditures.DeleteAllOnSubmit(delExpenditure);
+                        }
+
+                        context.SubmitChanges();
+                        MessageBox.Show("Expenditure entry deleted");
+                        NavigationService.Navigate(new Uri("/Views/Expenditure.xaml", UriKind.Relative));
+
+                    }
+                    else
+                        MessageBox.Show("This entry does not exist");
+                }
+            }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Expenditure.xaml", UriKind.Relative));
+        }
+    }
+}
